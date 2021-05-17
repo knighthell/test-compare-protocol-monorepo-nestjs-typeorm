3cd678118852299b68af8b02784b8a5d14848df1
knighthell <knighthell@gmail.com>
2021-05-17 11:20:18 +0900

    feat: revert graphql resolver

--- libs/place/src/entity/place.entity.ts
+++ libs/place/src/entity/place.entity.ts
@@ -1,24 +1,21 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { Point } from 'geojson';
 import { IsUUID } from 'class-validator';
-import { Field, ObjectType } from '@nestjs/graphql';
+import { ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 @Entity('place')
 export class PlaceEntity {
     @PrimaryGeneratedColumn('uuid')
-    @Field({ nullable: true })
     @IsUUID('4')
     id?: string;
 
     @Column({ type: 'varchar', length: 200 })
-    @Field({ nullable: false })
     name: string;
 
     @Column('geometry', {
         spatialFeatureType: 'Point',
         srid: 4326,
     })
-    @Field((type) => Point)
     geoPoint: Point;
 }
