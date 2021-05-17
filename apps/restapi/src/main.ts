d07554b0fd00084db53927e14bff0ac711bbc810
knighthell <knighthell@gmail.com>
2021-05-17 13:42:41 +0900

    fix: restful json request limit change to 50mb

--- apps/restapi/src/main.ts
+++ apps/restapi/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { RestapiModule } from './restapi.module';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(RestapiModule);
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
     await app.listen(3000);
     console.info('Test Place Restful API Service is listening');
 }
