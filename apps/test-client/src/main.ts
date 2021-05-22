f05d594cf0e3be5b71133854593e71488d2a7f54
knighthell <knighthell@gmail.com>
2021-05-22 12:14:49 +0900

    feat: update package-lock.json, docker-compose healthcheck bugfix

--- apps/test-client/src/main.ts
+++ apps/test-client/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { TestClientModule } from './test-client.module';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
     const app = await NestFactory.create(TestClientModule);
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
     await app.listen(3000);
+    console.info('Test Place Restful API Service is listening');
 }
 bootstrap();
